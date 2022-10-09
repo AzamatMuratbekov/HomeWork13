6686c0e80fb8d3c5643e6a57240f7afe31c4af92
Melloman <lovelyboyy@mail.ru>
2022-10-09 22:50:37 +0300

    i

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Class {
+public class Class {
+   private int number;
+   private String soz;
+   private int[] array;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getSoz() {
+        return soz;
+    }
+
+    public void setSoz(String soz) {
+        this.soz = soz;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public Class(int number, String soz, int[] array) {
+        this.number = number;
+        this.soz = soz;
+        this.array = array;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class klass = new Class(2,"IPS",new int[]{15,69,95});
+        System.out.println(klass.getNumber()+"\n"+klass.getSoz()+"\n"+ Arrays.toString(klass.getArray()));
+
     }
 }
